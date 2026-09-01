2324f06c24a055fc8423cefed2344abefeaaf747
CheFu <kurisanimaluleke77@gmail.com>
2026-09-01 18:32:23 +0200

    feat: integrate Sentry for full-stack error tracking and performance monitoring and update API base URL

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://934b5bc15e763b338e3963b9e8ac0e36@o4512011915296768.ingest.de.sentry.io/4512011933909072",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
